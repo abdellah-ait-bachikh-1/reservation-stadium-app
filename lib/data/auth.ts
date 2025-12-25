99984057d5a8ca5ebcdfa333f48615d2e61c1692
abdellah-ait-bachikh-1 <abdellahaitbachikh@gmail.com>
2025-12-25 11:36:45 +0100

    check user in server components lib functions

--- lib/data/auth.ts
+++ lib/data/auth.ts
@@ -7,19 +7,51 @@ export const isAuthenticated = async () => {
   if (!session || !session.user) {
     redirect("/auth/login");
   }
+  return session;
 };
 
 export const isDeletedUser = async () => {
-  const session = await getSession();
-  if (!session || !session.user) {
-    redirect("/auth/login");
-  }
+  const session = await isAuthenticated();
   const user = await db.user.findUnique({
     where: { id: session.user.id },
+    select: {
+      id: true,
+      fullNameAr: true,
+      fullNameFr: true,
+      approved: true,
+      email: true,
+      role: true,
+      preferredLocale: true,
+      deletedAt: true,
+      createdAt: true,
+      updatedAt: true,
+      phoneNumber: true,
+      emailVerifiedAt: true,
+    },
   });
   if (!user || user.deletedAt !== null) {
     await signOutServer();
     redirect("/auth/login");
+  }
+  if (!user.approved || !user.emailVerifiedAt) {
+    redirect(
+      `/${user.preferredLocale.toLocaleLowerCase()}/dashboard/profile/${
+        user.id
+      }`
+    );
+  }
+  return user;
+};
+
+export const isAdminUser = async () => {
+  const user = await isDeletedUser();
+
+  if (user.role !== "ADMIN") {
+    redirect(
+      `/${user?.preferredLocale.toLocaleLowerCase()}/dashboard/profile/${
+        user?.id
+      }`
+    );
   }
   return user;
 };
