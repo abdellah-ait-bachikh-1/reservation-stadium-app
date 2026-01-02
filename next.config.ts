111b14559b66b98947212a869242cfe5248bfdd1
abdellah-ait-bachikh-1 <abdellahaitbachikh@gmail.com>
2026-01-02 20:23:14 +0100

    reset app

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = { images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-        pathname: '/**',
-      }
-    ],
-  },};
- 
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
