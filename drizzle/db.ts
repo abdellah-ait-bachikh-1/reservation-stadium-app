e88440632b3f15ddc5c666a5850e96827833017f
abdellah-ait-bachikh-1 <abdellahaitbachikh@gmail.com>
2026-01-08 20:48:00 +0100

    schema : provide schema t drizzle

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,11 +1,11 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
-import { notifications, users } from "./schema";
+import { clubs, notifications, sports, users } from "./schema";
 
 // Create a MySQL connection pool
 const pool = mysql.createPool(process.env.DATABASE_URL!);
 
 export const db = drizzle(pool, {
-  schema: { users, notifications },
+  schema: { users, clubs, sports, notifications },
   mode: "default",
 });
